8ae969829dc1ac3137b7196efa17d36e4f65ffa8
megatechbuddy <seanalexanderbenson@gmail.com>
2019-07-04 12:35:46 -0500

    misc

--- Code/runnable.py
+++ Code/runnable.py
@@ -1,4 +1,98 @@
 from Analize_Car_Video.Code.display import App
 import tkinter
+<<<<<<< HEAD
 # Create a window and pass it to the Application object
 App(tkinter.Tk(), "Determine speed of the car.")
+=======
+from tkinter import Canvas
+import PIL.Image
+import PIL.ImageTk
+
+#######################################################################################
+# CONFIGURATION PARAMETERS
+input_video = './data/train.mp4'
+#######################################################################################
+# Methods and start
+
+# source: https://solarianprogrammer.com/2018/04/21/python-opencv-show-video-tkinter-window/
+#code is modified
+
+
+class App:
+    def __init__(self, window, window_title):
+        self.window = window
+        self.window.title(window_title)
+        self.video_source = input_video
+
+        # open video source (by default this will try to open the computer webcam)
+        self.vid = MyVideoCapture(self.video_source)
+
+        # Create a canvas that can fit the above video source size
+        self.canvas = tkinter.Canvas(
+            window, width=self.vid.width, height=self.vid.height)
+        self.canvas.pack()
+
+        # Button that lets the user take a snapshot
+        self.btn_snapshot = tkinter.Button(
+            window, text="Snapshot", width=50, command=self.snapshot)
+        self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=True)
+
+        # After it is called once, the update method will be automatically called every delay milliseconds
+        self.delay = 15
+        self.update()
+
+        self.window.mainloop()
+
+    def snapshot(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+
+        if ret:
+            cv.imwrite("./snapshots/frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") +
+                       ".jpg", cv.cvtColor(frame, cv.COLOR_RGB2BGR))
+
+    def update(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(
+                image=PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
+
+        self.window.after(self.delay, self.update)
+
+# source: https://solarianprogrammer.com/2018/04/21/python-opencv-show-video-tkinter-window/
+
+
+class MyVideoCapture:
+    def __init__(self, video_source=0):
+        # Open the video source
+        self.vid = cv.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
+
+        # Get video source width and height
+        self.width = self.vid.get(cv.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv.CAP_PROP_FRAME_HEIGHT)
+
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                # Return a boolean success flag and the current frame converted to BGR
+                return (ret, cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return (ret, None)
+
+    # Release the video source when the object is destroyed
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
+
+
+# Create a window and pass it to the Application object
+App(tkinter.Tk(), "Tkinter and OpenCV")
+>>>>>>> parent of 7f2c2f4... Shows orbs
